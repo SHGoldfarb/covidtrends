6fe3a55050ed27a997b362b6445efd684b39be3e
Samuel Goldfarb <shgoldfarbh@gmail.com>
2020-05-10 20:01:10 -0400

    add deaths

--- src/main.py
+++ src/main.py
@@ -8,74 +8,101 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 
 # constants
-data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master\
+cases_constants = {
+    "data_url": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master\
 /csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_\
-global.csv"
+global.csv",
+    "csv_file_name": "time_series_covid19_confirmed_global.csv",
+    "metadata_file_name": 'cases_meta.pickle'
+}
+
+deaths_constants = {
+    "data_url": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master\
+/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_\
+global.csv",
+    "csv_file_name": "time_series_covid19_deaths_global.csv",
+    "metadata_file_name": 'deaths_meta.pickle'
+}
+
 data_folder_name = 'data'
-csv_file_name = 'data.csv'
-metadata_file_name = 'meta.pickle'
 population_file_path = 'static_data/population.csv'
 stale_days = 0.1
 days_on_last_sum = 7
 
-data_file_path = os.path.join(data_folder_name, csv_file_name)
-metadata_file_path = os.path.join(data_folder_name, metadata_file_name)
 stale_timedelta = timedelta(days=stale_days)
+
+
+def in_data_folder(file_name):
+    return os.path.join(data_folder_name, file_name)
 
 
 def create_data_folder():
     Path(data_folder_name).mkdir(parents=True, exist_ok=True)
 
 
-def get_metadata():
-    if not os.path.exists(metadata_file_path):
+def get_metadata(file_path):
+    if not os.path.exists(file_path):
         return {}
-    with open(metadata_file_path, 'rb') as metadata_file:
+    with open(file_path, 'rb') as metadata_file:
         return pickle.load(metadata_file)
 
 
-def save_metadata(metadata):
-    with open(metadata_file_path, 'wb') as metadata_file:
+def save_metadata(metadata, file_path):
+    with open(file_path, 'wb') as metadata_file:
         pickle.dump(metadata, metadata_file)
 
 
-def update_metadata(new_metadata):
-    metadata = get_metadata()
-    save_metadata({**metadata, **new_metadata})
+def update_metadata(new_metadata, file_path):
+    metadata = get_metadata(file_path)
+    save_metadata({**metadata, **new_metadata}, file_path)
 
 
 def update_data():
+
     create_data_folder()
+    for constants in [cases_constants, deaths_constants]:
+        metadata_file_path = in_data_folder(constants["metadata_file_name"])
+        csv_file_path = in_data_folder(constants["csv_file_name"])
 
-    # If data is stale
-    metadata = get_metadata()
-    date = metadata.get('date')
-    now = datetime.now()
-    if date and (date + stale_timedelta > now):
-        print('Data up to date, skipping download.')
-        return
+        # If data is stale
+        metadata = get_metadata(metadata_file_path)
+        date = metadata.get('date')
+        now = datetime.now()
+        if date and (date + stale_timedelta > now):
+            print('Data up to date, skipping download.')
+            continue
 
-    print('Data stale or unexistent, downloading...')
-    # Get and save data
-    request_result = requests.get(data_url, allow_redirects=True)
+        print('Data stale or unexistent, downloading...')
+        # Get and save data
+        request_result = requests.get(
+            constants["data_url"], allow_redirects=True)
 
-    open(data_file_path,
-         'wb').write(request_result.content)
-    print('data updated')
+        open(csv_file_path,
+             'wb').write(request_result.content)
+        print('data updated')
 
-    # Update data staleness
-    update_metadata({'date': now})
+        # Update data staleness
+        update_metadata({'date': now}, metadata_file_path)
 
 
 def sanitize_data(data):
-    return data.drop(
+    # Group countries
+    data = data.groupby(by=['Country/Region']).sum()
+
+    # Remove unwanted "countries"
+    data = data.drop(
         ['West Bank and Gaza', 'Diamond Princess', 'Kosovo', 'MS Zaandam']
     )
 
+    # Remove lat long
+    data = data.iloc[:, 2:]
 
-def get_data():
+    return data
+
+
+def get_data(file_path):
     update_data()
-    data = pd.read_csv(data_file_path)
+    data = pd.read_csv(file_path)
     return data
 
 
@@ -83,27 +110,20 @@ def get_population():
     return pd.read_csv(population_file_path).set_index('Country')
 
 
-def show_charts():
-    data = get_data()
+def show_charts(data):
 
-    # Group countries
-    data = data.groupby(by=['Country/Region']).sum()
-
-    # Remove unwanted "countries"
     data = sanitize_data(data)
 
-    # Remove lat long
-    data = data.iloc[:, 2:]
-
     # Create new dataframe with last cases
-    def new_cases_last_days(row):
+
+    def new_last_days(row):
         cases = row[1]
 
         def get_new(i):
             return cases[i] - cases[max(0, i - days_on_last_sum)]
         return map(get_new, range(len(cases)))
 
-    data_new = pd.DataFrame([new_cases_last_days(row)
+    data_new = pd.DataFrame([new_last_days(row)
                              for row in data.iterrows()],
                             data.index,
                             data.columns)
@@ -127,11 +147,12 @@ def show_charts():
     # Plot both dataframes
     _, ax = plt.subplots()
     for i in range(len(data.index)):
-        if data.iloc[i].name in ['Chile', 'US', 'Switzerland',
-                                 'Spain', 'Italy', 'France', 'Germany'
+        if data.iloc[i].name in ['Chile', 'Argentina', 'Germany',
+                                 'United Kingdom', 'Ecuador', 'US',
+                                 'Brazil', 'Italy', 'Spain', 'Australia',
                                  ]:
-            ax.plot(data.iloc[i, :],
-                    data_new.iloc[i, :], label=data.index[i])
+            ax.loglog(data.iloc[i, :],
+                      data_new.iloc[i, :], label=data.index[i])
     ax.get_yaxis().set_major_formatter(
         ticker.FuncFormatter(lambda x, p: str(round(x, 4))))
     ax.get_xaxis().set_major_formatter(
@@ -144,8 +165,24 @@ def show_charts():
     plt.show()
 
 
+def show_cases_chart():
+    file_path = in_data_folder(cases_constants["csv_file_name"])
+
+    data = get_data(file_path)
+
+    show_charts(data)
+
+
+def show_deaths_chart():
+    file_path = in_data_folder(deaths_constants["csv_file_name"])
+
+    data = get_data(file_path)
+
+    show_charts(data)
+
+
 def main():
-    show_charts()
+    show_deaths_chart()
 
 
 if __name__ == "__main__":
